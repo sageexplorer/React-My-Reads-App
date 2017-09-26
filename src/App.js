80e3f8acec683dd7d15a26f08e19125eae37a11b
sageexplorer <srimal@campusexplorer.com>
2017-09-26 15:03:54 -0700

    Update App.js
    no more white spaces

--- src/App.js
+++ src/App.js
@@ -12,37 +12,23 @@ class BooksApp extends React.Component {
     render() {
         return (
             <div className="app">
-
-
+            
                 <Route path='/search' render={() => (
-
                     <SearchResults  />
-
                 )}/>
 
-
                 <Route exact path='/' render={() => (
-
-
                     <div>
-
                         <div className="list-books-title">
-
                             <h2>My Reads</h2>
-
                         </div>
-
                         <AddToBookShelf type="currentlyReading" title="Currently Reading"/>
 
                         <AddToBookShelf type="wantToRead" title="Want To Read"/>
 
                         <AddToBookShelf type="read" title="Read"/>
-
-
                     </div>
-
                 )}/>
-
             </div>
         )
     }
